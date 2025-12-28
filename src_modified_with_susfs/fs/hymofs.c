899f2333667da963dbf449cb02dc1783cb4442d5
Bruno Ancona <brunoanconasala@gmail.com>
2025-12-28 00:16:13 -0600

    Fix errors when compiling with bazel

--- src_modified_with_susfs/fs/hymofs.c
+++ src_modified_with_susfs/fs/hymofs.c
@@ -362,7 +362,7 @@ static int hymo_dispatch_cmd(unsigned int cmd, void __user *arg) {
     struct hymo_inject_entry *inject_entry;
     char *src = NULL, *target = NULL;
     u32 hash;
-    unsigned long flags;
+    unsigned long flags __maybe_unused;
     bool found = false;
     int ret = 0;
 
@@ -1591,8 +1591,8 @@ static __always_inline bool bloom_test(const unsigned long *filter, const char *
 {
     u32 h1 = full_name_hash(NULL, name, namlen);
     /* Use bitwise AND to avoid branch */
-    return test_bit(h1 & HYMO_BLOOM_MASK, filter) & 
-           test_bit((h1 >> 16) & HYMO_BLOOM_MASK, filter);
+    return (int)test_bit(h1 & HYMO_BLOOM_MASK, filter) & 
+           (int)test_bit((h1 >> 16) & HYMO_BLOOM_MASK, filter);
 }
 
 /* Callback context for enumerating merge target directory */
@@ -2069,7 +2069,7 @@ bool hymofs_is_overlay_xattr(struct dentry *dentry, const char *name)
 {
     struct hymo_xattr_sb_entry *sb_entry;
     bool found = false;
-    unsigned long flags;
+    unsigned long flags __maybe_unused;
 
     if (!name) return false;
     if (strncmp(name, "trusted.overlay.", 16) != 0) return false;
